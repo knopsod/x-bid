ce08ae0d388b81ddf8dded85a932cd27add3f08e
Noppadon Sodram <knopsod@gmail.com>
2018-06-08 00:19:41 +0700

    First commit init

---
+++ imports/ui/pages/Transactions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Transactions = () => (
+  <div className="container">
+    This is Transactions()
+    <Button bsStyle="primary">Ok react+meteor+bootstrap style</Button>
+  </div>
+);
+
+export default Transactions;
